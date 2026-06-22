4f7427aa049075e9f35f4ea13e686130e442c258
James <james.m.topmiller@gmail.com>
2026-06-22 14:38:43 -0700

    ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,29 @@ import AboutPage from './pages/AboutPage';
 import ShowsPage from './pages/ShowsPage';
 import MusicPage from './pages/MusicPage';
 import ContactPage from './pages/ContactPage';
+import ScrollToTop from "./components/ScrollToTop";
+
 
 
 export default function App() {
 
   return (
-    <div className="app">
-      <Navbar />
+    <>
+      <div className="app">
+        <Navbar />
 
-      <main className="main">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/shows" element={<ShowsPage />} />
-          <Route path="/music" element={<MusicPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </main>
-      
-    </div>
+        <main className="main">
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/shows" element={<ShowsPage />} />
+            <Route path="/music" element={<MusicPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Routes>
+        </main>
+
+      </div>
+    </>
   );
 }
